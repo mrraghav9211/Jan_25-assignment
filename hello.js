77f35f292646baa87bf6dd1114f9f3325190830f
Rajani Kant Raghav <82476053+mrraghav9211@users.noreply.github.com>
2022-01-26 13:53:26 +0530

    Assignment 1:

---
+++ hello.js
@@ -0,0 +1,13 @@
+// javascript section
+
+var n = prompt("enter any number");
+
+if (n>=10) {
+	console.log("number is grater then or equal to 10");
+	document.write("number is grater then or equal to 10");
+}   
+else
+{
+	console.log("number is not grater then or equal to 10");
+	document.write("number is not grater then or equal to 10");
+}
