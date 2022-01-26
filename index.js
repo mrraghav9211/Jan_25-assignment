dba69afd96e2ea90154c2e54bf9fac1c665f3cca
Rajani Kant Raghav <82476053+mrraghav9211@users.noreply.github.com>
2022-01-26 13:54:19 +0530

    Assignment 2

---
+++ index.js
@@ -0,0 +1,13 @@
+var a= prompt("enter a");
+var b= prompt("enter b");
+var c= prompt("enter c");
+
+if (a>b && a>c) {
+	document.write("a is maximun");
+}
+else if (b>a && b>c) {
+	document.write("b is maximun");
+} else
+{
+document.write("c is maximun");
+}
